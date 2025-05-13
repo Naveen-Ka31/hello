3b4964c6293b03cd5838867abbbf60f3b5bfe0ee
Naveen-Ka31 <153277803+Naveen-Ka31@users.noreply.github.com>
2025-05-13 20:57:32 +0530

    Update App.java

--- src/main/java/com/example/helloworld/App.java
+++ src/main/java/com/example/helloworld/App.java
@@ -4,6 +4,6 @@ package com.example.helloworld;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Maven java program from github hello!");
     }
 }
